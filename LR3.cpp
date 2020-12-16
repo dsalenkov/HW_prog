a760777558dafe91527bcfe2984edac231f02e9c
dsalenkov <d.salenkoff@ayndex.ru>
2020-12-16 19:02:38 +0300

    Update LR3.cpp

--- LR3.cpp
+++ LR3.cpp
@@ -2,15 +2,16 @@
 
 int main(){
     int n, k = 0;
-    std::cin >> n; // ввод колчиества точек
-    double X[n], Y[n], R; // массив для координат х и у, радиус
+    std::cin >> n; // number of points
+    double X[n], Y[n], R; // array of cordinates and radius
     for(int i = 0; i < n; i++)
-        std::cin >> X[i] >> Y[i]; // ввод массива
-    double x0, y0; // центра круга
-    std::cin >> x0 >> y0 >> R; // ввод центра круга и радиуса
+        std::cin >> X[i] >> Y[i];
+    double x0, y0; // centre of a circle
+    std::cin >> x0 >> y0 >> R;
     for(int i = 0; i < n; i++){
-        if((X[i] - x0)*(X[i] - x0)+ (Y[i] - y0)*(Y[i] - y0) >= R^2) // если расстояние больше радиуса, то k++
+        if((X[i] - x0)*(X[i] - x0)+ (Y[i] - y0)*(Y[i] - y0) >= R^2) // Pifagor`s theorem: if the distance between centre and point is more than radius, then k++
             k++;
     }
     std::cout << k;
+    return 0;
 }
