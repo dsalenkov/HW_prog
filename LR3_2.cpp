b21f46ec9078b3fe72c1ed800f3e82e19c5408e7
dsalenkov <d.salenkoff@ayndex.ru>
2020-12-16 19:13:20 +0300

    Update LR3_2.cpp

--- LR3_2.cpp
+++ LR3_2.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 
 int main(){
-    int N, M;
+    int N, M; // size of array
     std::cin >> N >> M;
-    int ar[N][M];
+    int ar[N][M]; // array
     for(int i = 0; i < N; i ++){
         for(int j = 0; j < M; j++){
             std::cin >> ar[i][j];
         }
     }
 
-    for(int i = 0; i < N; i ++){
+    for(int i = 0; i < N; i ++){ // check the negative numbers
         for(int j = 0; j < M; j++){
-            if(ar[i][j] < 0)
+            if(ar[i][j] < 0 && i != 0)
                 ar[i][j] += ar[i-1][j];
         }
     }
