9ba8a0f5e674890fcac39fbe050c7ec39b1e27f6
dsalenkov <d.salenkoff@ayndex.ru>
2020-12-16 18:59:33 +0300

    Update LR3.cpp

--- LR3.cpp
+++ LR3.cpp
@@ -2,14 +2,14 @@
 
 int main(){
     int n, k = 0;
-    std::cin >> n;
-    double X[n], Y[n], R;
+    std::cin >> n; // ввод колчиества точек
+    double X[n], Y[n], R; // массив для координат х и у, радиус
     for(int i = 0; i < n; i++)
-        std::cin >> X[i] >> Y[i];
-    double x0, y0;
-    std::cin >> x0 >> y0 >> R;
+        std::cin >> X[i] >> Y[i]; // ввод массива
+    double x0, y0; // центра круга
+    std::cin >> x0 >> y0 >> R; // ввод центра круга и радиуса
     for(int i = 0; i < n; i++){
-        if((X[i] - x0)*(X[i] - x0)+ (Y[i] - y0)*(Y[i] - y0) >= R^2)
+        if((X[i] - x0)*(X[i] - x0)+ (Y[i] - y0)*(Y[i] - y0) >= R^2) // если расстояние больше радиуса, то k++
             k++;
     }
     std::cout << k;
